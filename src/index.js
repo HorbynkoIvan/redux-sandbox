d0256e3530fac3a3dc31c1ca657b768005e96633
HorbynkoIvan <horbynko.i@gmail.com>
2020-06-12 18:13:47 +0300

    simple counter inc dec clr

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import {createStore} from "redux";
 
-const reducer = (state = 0, action) =>{
+const reducer = (state = 0, action) => {
 
     switch (action.type) {
+        case 'DEC':
+            return state - 1;
         case 'INC':
             return state + 1;
+        case 'CLR':
+            return state = 0;
         default:
             return state
     }
@@ -13,7 +17,16 @@ const reducer = (state = 0, action) =>{
 
 const store = createStore(reducer);
 
-store.subscribe(() => console.log(store.getState()))
+const counter = document.getElementById('counter');
+const decBtn = document.getElementById('dec');
+const incBtn = document.getElementById('inc');
+const clrBtn = document.getElementById('clr');
 
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
+decBtn.addEventListener('click',() => store.dispatch({type: 'DEC'}))
+incBtn.addEventListener('click',() => store.dispatch({type: 'INC'}))
+clrBtn.addEventListener('click',() => store.dispatch({type: 'CLR'}))
+
+store.subscribe(() => {
+    console.log(store.getState());
+    counter.innerHTML = store.getState();
+})

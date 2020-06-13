f919e4e17f08ad4ba6891f7d538483c98871f7db
HorbynkoIvan <horbynko.i@gmail.com>
2020-06-13 11:37:36 +0300

    added btn RND

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ const reducer = (state = 0, action) => {
             return state - 1;
         case 'INC':
             return state + 1;
+        case 'RND':
+            return state = action.payload;
         case 'CLR':
             return state = 0;
         default:
@@ -20,11 +22,16 @@ const store = createStore(reducer);
 const counter = document.getElementById('counter');
 const decBtn = document.getElementById('dec');
 const incBtn = document.getElementById('inc');
+const rndBtn = document.getElementById('rnd');
 const clrBtn = document.getElementById('clr');
 
-decBtn.addEventListener('click',() => store.dispatch({type: 'DEC'}))
-incBtn.addEventListener('click',() => store.dispatch({type: 'INC'}))
-clrBtn.addEventListener('click',() => store.dispatch({type: 'CLR'}))
+decBtn.addEventListener('click', () => store.dispatch({type: 'DEC'}))
+incBtn.addEventListener('click', () => store.dispatch({type: 'INC'}))
+rndBtn.addEventListener('click', () => {
+    const random = Math.floor(Math.random() * 10)
+    store.dispatch({type: 'RND', payload: random})
+})
+clrBtn.addEventListener('click', () => store.dispatch({type: 'CLR'}))
 
 store.subscribe(() => {
     console.log(store.getState());

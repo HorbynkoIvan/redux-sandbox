a4593e84602f759c5d795f96196b6c907bc53d9b
HorbynkoIvan <horbynko.i@gmail.com>
2020-06-13 11:59:40 +0300

    simple actioncreater

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import {createStore} from "redux";
 
+const dec = () => ({type: 'DEC'});
+const inc = () => ({type: 'INC'});
+const rnd = (payload) => ({type: 'RND', payload});
+const clr = () => ({type: 'CLR'});
+
 const reducer = (state = 0, action) => {
 
     switch (action.type) {
@@ -25,13 +30,13 @@ const incBtn = document.getElementById('inc');
 const rndBtn = document.getElementById('rnd');
 const clrBtn = document.getElementById('clr');
 
-decBtn.addEventListener('click', () => store.dispatch({type: 'DEC'}))
-incBtn.addEventListener('click', () => store.dispatch({type: 'INC'}))
+decBtn.addEventListener('click', () => store.dispatch(dec()))
+incBtn.addEventListener('click', () => store.dispatch(inc()))
 rndBtn.addEventListener('click', () => {
     const random = Math.floor(Math.random() * 10)
-    store.dispatch({type: 'RND', payload: random})
+    store.dispatch(rnd(random))
 })
-clrBtn.addEventListener('click', () => store.dispatch({type: 'CLR'}))
+clrBtn.addEventListener('click', () => store.dispatch(clr()))
 
 store.subscribe(() => {
     console.log(store.getState());

bb58eb9a6b8cd5ed1fd09c491ca20db713fbaaba
HorbynkoIvan <horbynko.i@gmail.com>
2020-06-14 19:27:42 +0300

    connected component with redux (connect, mapStateToProps, mapDispatchToProps)

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,34 +1,30 @@
 import React from "react";
+import {connect} from 'react-redux'
+import {bindActionCreators} from "redux";
+
+import * as actions from "../actions";
 
 const Counter = ({counter, inc, dec, rnd, clr}) => {
     return (
         <div className="jumbotron">
             <h2 id="counter">{counter}</h2>
             <button
-                id="dec"
-                type="button"
                 className="btn btn-primary btn-lg mx-1"
                 onClick={dec}
             >
                 DEC
             </button>
             <button
-                id="inc"
-                type="button"
                 className="btn btn-secondary btn-lg mx-1"
                 onClick={inc}>
                 INC
             </button>
             <button
-                id="rnd"
-                type="button"
                 className="btn btn-success btn-lg mx-1"
                 onClick={rnd}>
                 RND
             </button>
             <button
-                id="clr"
-                type="button"
                 className="btn btn-warning btn-lg mx-1"
                 onClick={clr}>
                 CLR
@@ -37,4 +33,23 @@ const Counter = ({counter, inc, dec, rnd, clr}) => {
     )
 }
 
-export default Counter;
+const mapStateToProps = (state) => {
+    return {
+        counter: state
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    const {inc, dec, rnd, clr} = bindActionCreators(actions, dispatch);
+    return {
+        dec,
+        inc,
+        rnd: () => {
+            const random = Math.floor(Math.random() * 10);
+            rnd(random);
+        },
+        clr,
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- src/index.js
+++ src/index.js
@@ -1,32 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, bindActionCreators} from "redux";
+import {Provider} from 'react-redux';
+import {createStore} from "redux";
 import reducer from "./reducer";
-import * as actions from "./actions";
 
-import Counter from "./components/counter";
+import App from "./components/app";
 
 const store = createStore(reducer);
-const {dispatch} = store;
-const {dec, inc, rnd, clr} = bindActionCreators(actions, dispatch);
 
-const update = () => {
-    return ReactDOM.render(
-        <Counter
-            counter={store.getState()}
-            dec={dec}
-            inc={inc}
-            rnd={() => {
-                const random = Math.floor(Math.random() * 20);
-                rnd(random);
-            }}
-            clr={clr}
-        />,
-        document.getElementById('root'))
-}
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root'))
 
-update();
-store.subscribe(() => {
-    update();
-})
 
